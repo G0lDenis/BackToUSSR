5b4d4034da8449cff6853db97e40ce4d6d87319c
PavelShilinRussia <48483201+PavelShilinRussia@users.noreply.github.com>
2021-01-16 21:16:51 +0300

    частично реализована механика стрельбы

--- main.py
+++ main.py
@@ -53,9 +53,63 @@ class Enemy:
     pass
 
 
+class Weapon:
+    def __init__(self, name, damage, radius, img_path):
+        self.name = name
+        self.damage = damage
+        self.radius = radius
+        im = pygame.image.load(img_path)
+        self.spr = pygame.sprite.Sprite()
+        self.spr.image = im
+        self.spr.rect = self.spr.image.get_rect()
+        all_sprites.add(self.spr)
+        self.spr.rect.x, self.spr.rect.y = (10, screen.get_height() - 100)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, weapon, dest_x, dest_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.image = pygame.Surface((5, 5))
+        self.image.fill('yellow')
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.dest_x = dest_x
+        self.dest_y = dest_y
+        if dest_x > hero.rect.x:
+            self.speed_x = 10
+        else:
+            self.speed_x = -10
+        self.weapon = weapon
+        self.find_path()
+
+    def update(self):
+        self.rect.y += self.speed_y
+        self.rect.x += self.speed_x
+        square_go_x = math.fabs(self.rect.x - self.dest_x) ** 2
+        square_go_y = math.fabs(self.rect.y - self.dest_y) ** 2
+        if self.rect.bottom < 0 or self.rect.right < 0 or self.rect.top > screen.get_height() or self.rect.left > screen.get_width():
+            print('kill')
+            self.kill()
+
+    def find_path(self):
+        len_x = self.dest_x - self.x
+        len_y = self.dest_y - self.y
+        points_up = len_x // self.speed_x
+        if points_up != 0:
+            self.speed_y = len_y / points_up
+        else:
+            self.speed_x = 0
+        print(self.speed_y)
+
+
 class MainHero(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
+        self.weapons = [Weapon('simple pistol', 10, 200, 'Images/default_pistol.png')]
+        self.slot_number = 0
         self.pos = pos
         self.side = 'right'
         self.cur_frame = 0
@@ -74,6 +128,7 @@ class MainHero(pygame.sprite.Sprite):
 
     def update_hero(self):
         next_x, next_y = 0, 0
+
         if pygame.key.get_pressed()[pygame.K_a]:
             next_x -= 9
             self.side = 'left'
@@ -97,15 +152,27 @@ class MainHero(pygame.sprite.Sprite):
         self.rect = self.rect.move(next_x, next_y)
         if pygame.sprite.spritecollideany(self, obstacles) is not None or not self.in_field():
             self.rect = self.rect.move(-next_x, -next_y)
+        self.image = self.frames[self.cur_frame]
         self.rotate_image(pygame.mouse.get_pos())
         self.image.set_colorkey((255, 255, 255))
         self.moving = False
 
     def in_field(self):
-        pass
+        if self.rect.x < 0 or self.rect.x + self.rect.w > 1024 or \
+                self.rect.y < 0 or self.rect.y + self.rect.h > 768:
+            return False
+        return True
 
     def rotate_image(self, pos):
-        pass
+        rel_x, rel_y = pos[0] - self.rect.x, pos[1] - self.rect.y
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        self.image = pygame.transform.rotate(self.image, int(angle))
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def shoot(self, go_to):
+        bullet = Bullet(self.rect.centerx, self.rect.top, hero.weapons[hero.slot_number], go_to[0], go_to[1])
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
 
 class Game:
@@ -115,6 +182,9 @@ class Game:
 
     def render(self):
         self.hero.update_hero()
+        for i in bullets:
+            i.update()
+        print(bullets)
         self.room.render()
         all_sprites.draw(screen)
 
@@ -127,6 +197,7 @@ if __name__ == '__main__':
     pygame.display.set_caption('Back To USSR')
     all_sprites = pygame.sprite.Group()
     obstacles = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
     room = Room('f-r.txt')
     hero = MainHero((0, 3 * room.tile_height))
     game = Game(room, hero)
@@ -136,6 +207,8 @@ if __name__ == '__main__':
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
                 terminate()
+            elif ev.type == pygame.MOUSEBUTTONDOWN:
+                hero.shoot(pygame.mouse.get_pos())
         screen.fill((0, 0, 0))
         game.render()
         pygame.display.flip()
